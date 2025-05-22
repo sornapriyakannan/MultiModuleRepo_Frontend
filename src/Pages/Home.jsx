d4af401cb09f692f3fc96166e0ec8674a74c63a4
sornapriyakannan <sornapriya2004@gmail.com>
2025-05-22 07:10:24 +0530

    frontend update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default Home
+
+//without backend and db
+//npm i -g json-server@0.17.1
+//npx json-server --watch kec.json 3000
